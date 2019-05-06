fa843849dcdfb17309e5e9c37e6338faee60edf4
Guye Raphael, I233 <raphael.guye@post.ch>
2019-05-06 09:24:04 +0200

    Implementing a searchbar that run the search on a list everytime a key is pressed

---
+++ SearchWithReactiveUI/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SearchWithReactiveUI.ViewModels;
+using Xamarin.Forms;
+
+namespace SearchWithReactiveUI
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainPageViewModel();
+        }
+
+        void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            (BindingContext as MainPageViewModel).SearchCommand.Execute(null);
+        }
+    }
+}
---
+++ SearchWithReactiveUI/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SearchWithReactiveUI.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private readonly ObservableCollection<string> items;
+
+        public MainPageViewModel()
+        {
+            items = new ObservableCollection<string> { "John", "Tom", "Alex", "Tomas", "Alexandre", "Johnatan", "Jonas", "Joel", "James" };
+
+            ItemsDisplayed = items; // Initial value
+
+            SearchCommand = new Command(SearchCommandExecute);
+        }
+
+        private string searchText;
+        public string SearchText
+        {
+            get => searchText;
+            set
+            {
+                searchText = value;
+                OnPropertyChanged(nameof(SearchText));
+            }
+        }
+
+        private ObservableCollection<string> itemsDisplayed;
+        public ObservableCollection<string> ItemsDisplayed
+        {
+            get => itemsDisplayed;
+            set
+            {
+                itemsDisplayed = value;
+                OnPropertyChanged(nameof(ItemsDisplayed));
+            }
+        }
+
+        public ICommand SearchCommand { get; private set; }
+
+        private void SearchCommandExecute()
+        {
+            if(string.IsNullOrEmpty(SearchText))
+            {
+                ItemsDisplayed = items;
+            }
+            else
+            {
+                var lowerSearch = SearchText.ToLower();
+                var listResult = items.Where(item => item.ToLower().Contains(lowerSearch)).ToList();
+                ItemsDisplayed = new ObservableCollection<string>(listResult);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
